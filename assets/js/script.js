8bc7906990e90527b8fa2ced8c40eacfcfbef1e9
Brian Joe Antoni <brianjoeantoni@gmail.com>
2023-11-08 09:11:01 +0700

    dropdown language option fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
-function show() {
-  let language = document.querySelector(".language");
-  language.classList.toggle("hidden");
+function dropLanguage() {
+  document.getElementById("container-dropdown").classList.toggle("block");
 }
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("block")) {
+        openDropdown.classList.remove("block");
+      }
+    }
+  }
+};
